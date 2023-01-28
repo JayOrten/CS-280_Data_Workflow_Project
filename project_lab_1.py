015d9a00d1e5b4a342e71f9ea399499d09e52cd7
JayOrten <jayorten@Jay-Orten-cs280.myguest.virtualbox.org>
2023-01-28 12:44:02 -0700

    add load task

--- project_lab_1.py
+++ project_lab_1.py
@@ -11,13 +11,13 @@ import json
 from google.cloud import storage
 from gcsfs import GCSFileSystem
 import os
+from databox import Client
 
 def get_auth_header(my_bearer_token):
 	return {"Authorization": f"Bearer {my_bearer_token}"}
 
 def get_twitter_api_data_task_func(ti: TaskInstance, **kwargs):
 	# Pulls bearer token
-	print("test")
 	bearer_token = Variable.get("Bearer Token")
 
 	# Create authentication header
@@ -69,10 +69,11 @@ def transform_twitter_api_data(ti: TaskInstance, **kwargs):
 	user_requests_df = pd.json_normalize(json.loads(user_requests_df.to_json(orient='records')))
 
 	# Drop columns
-	user_requests_df = user_requests_df.drop(user_requests_df.columns[[0,7]], axis=1)
+	user_requests_df.drop(['data.data.profile_image_url', 'data.data.description'], axis=1)
 
 	# Rename
-	user_requests_df.columns = ['username','user_id','followers_count','following_count','tweet_count','listed_count','name']
+	user_requests_df = user_requests_df[['data.data.id','data.data.username','data.data.name','data.data.public_metrics.followers_count','data.data.public_metrics.following_count','data.data.public_metrics.tweet_count','data.data.public_metrics.listed_count']]
+	user_requests_df.columns = ['user_id','username','name','followers_count','following_count','tweet_count','listed_count']
 
 
 	# Transform tweet data
@@ -85,10 +86,12 @@ def transform_twitter_api_data(ti: TaskInstance, **kwargs):
 	tweet_requests_df = pd.json_normalize(json.loads(tweet_requests_df.to_json(orient='records')))
 
 	# Drop columns
-	tweet_requests_df = tweet_requests_df.drop(tweet_requests_df.columns[[0,7]], axis=1)
+	tweet_requests_df.drop(['data.data.author_id', 'data.data.edit_history_tweet_ids'], axis=1)
 
 	# Rename
-	tweet_requests_df.columns = ['retweet_count','reply_count','like_count','quote_count','impression_count','text','tweet_id',]
+	tweet_requests_df = tweet_requests_df[['data.data.id','data.data.text','data.data.public_metrics.retweet_count','data.data.public_metrics.reply_count','data.data.public_metrics.like_count','data.data.public_metrics.quote_count','data.data.public_metrics.impression_count']]
+	tweet_requests_df.columns = ['tweet_id','text','retweet_count','reply_count','like_count','quote_count','impression_count']
+
 
 	os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/jay_orten/airflow-cs280/auth/bucket_auth.json"
 
@@ -96,6 +99,51 @@ def transform_twitter_api_data(ti: TaskInstance, **kwargs):
 	bucket = client.get_bucket("j-o-apache-airflow-cs280")
 	bucket.blob("data/user_data.csv").upload_from_string(user_requests_df.to_csv(index=False), "text/csv")
 	bucket.blob("data/tweet_data.csv").upload_from_string(tweet_requests_df.to_csv(index=False), "text/csv")
+
+	return
+
+def load_twitter_api_data(ti: TaskInstance, **kwargs):
+
+	os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/jay_orten/airflow-cs280/auth/bucket_auth.json"
+
+	tweet_data
+	user_data
+
+	fs = GCSFileSystem(project="Jay-Orten-CS-280")
+	with fs.open('gs://j-o-apache-airflow-cs280/data/tweet_data.csv', 'rb') as f:
+		tweet_data = pd.read_csv(f)
+
+	with fs.open('gs://j-o-apache-airflow-cs280/data/user_data.csv', 'rb') as f:
+		user_data = pd.read_csv(f)
+
+	user_token = Variable.get("DATABOX_TOKEN")
+
+	dbox = Client(user_token)
+
+	for index, row in tweet_data.iterrows():
+		reply_count = row["reply_count"]
+		like_count = row["like_count"]
+		favorite_count = row["quote_count"]
+		retweet_count = row["retweet_count"]
+		tweet_id = row["tweet_id"]
+
+		dbox.push((tweet_id + "_reply_count"),reply_count)
+		dbox.push((tweet_id + "_like_count"),like_count)
+		dbox.push((tweet_id + "_favorite_count"),favorite_count)
+		dbox.push((tweet_id + "_retweet_count"),retweet_count)
+
+	for index, row in user_data.iterrows():
+		followers_count = row["followers_count"]
+		following_count = row["following_count"]
+		tweet_count = row["tweet_count"]
+		listed_count  = row["listed_count"]
+		name = row["name"]
+
+		dbox.push((name + "_followers_count"),followers_count)
+		dbox.push((tweet_id + "_following_count"),following_count)
+		dbox.push((tweet_id + "_tweet_count"),tweet_count)
+		dbox.push((tweet_id + "_listed_count"),listed_count)
+	
 
 	return
 
@@ -116,7 +164,12 @@ with DAG(
 		python_callable=transform_twitter_api_data,
 		provide_context=True
 		)
+	load_twitter_api_data_task = PythonOperator(
+		task_id="load_twitter_api_data",
+		python_callable=load_twitter_api_data,
+		provide_context=True
+		)
 	end_task = DummyOperator(task_id="end_task")
 
 
-start_task >> get_twitter_api_data_task >> transform_twitter_api_data_task >> end_task
+start_task >> get_twitter_api_data_task >> transform_twitter_api_data_task >> load_twitter_api_data_task >> end_task
