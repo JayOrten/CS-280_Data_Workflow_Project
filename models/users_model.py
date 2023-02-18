90fbbac7ea6813d7c1afe3b72e9caae45e00b598
JayOrten <jayorten@Jay-Orten-cs280.myguest.virtualbox.org>
2023-02-18 14:30:48 -0700

    add boilerplate

---
+++ models/users_model.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, Date, Float
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, not_null=True)
+    twitter_user_id = Column(Integer, not_null=True)
+    username = Column(String, not_null=True)
+    
+   def __repr__(self) -> str:
+        return f"User(id={self.id}, twitter_user_id={self.twitter_user_id}, username={self.username})" 
