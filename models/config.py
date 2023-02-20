67b0eb0dea3bd018271feab95dc7b270dd5174ce
JayOrten <jayorten@Jay-Orten-cs280.myguest.virtualbox.org>
2023-02-20 15:40:30 -0700

    fix config file

--- models/config.py
+++ models/config.py
@@ -3,9 +3,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 def create_dburl():
-  hostname = Variable.get("DATABASE_USERNAME")
-  username = Variable.get("DATABASE_PASSWORD")
-  password = Variable.get("DATABASE_HOSTNAME")
+  hostname = Variable.get("DATABASE_HOSTNAME")
+  username = Variable.get("DATABASE_USERNAME")
+  password = Variable.get("DATABASE_PASSWORD")
   port = Variable.get("DATABASE_PORT")
   database = Variable.get("DATABASE_NAME")
   return f"postgresql+psycopg2://{username}:{password}@{hostname}:{port}/{database}"
