24170507739341cee6ea70481d6bc2eb018e9d10
Jay <jay.orten@gmail.com>
2023-04-06 18:00:53 -0600

    add rate limiting

--- Tool-Lab-4/main.py
+++ Tool-Lab-4/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from fastapi import File, UploadFile
+from slowapi.errors import RateLimitExceeded
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from starlette.requests import Request
+from starlette.responses import Response
 import tensorflow as tf
 from tensorflow import keras
 from PIL import Image
@@ -7,14 +12,21 @@ from io import BytesIO
 import numpy as np
 import cv2
 
+limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
+app.state.limiter = limiter # Define's the api's limiter object
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler) # Adds the exception to the api 
+
+
 # Home route
 @app.get("/")
+@limiter.limit("15/minute")
 async def root():
 	return {"api_info": "Jay - Orten's CS280 Image Classifier"}
 
 # Classify image:
 @app.post("/classify/")
+@limiter.limit("5/minute")
 async def classify(file: UploadFile = File(...)):
     # Recieve image
     fileString = file.filename
